b8ea2d4a03dbebe613d51e3a8df266422dca160c
ccbccbccb <141594822+ccbccbccb@users.noreply.github.com>
2023-09-27 13:01:07 +0900

    09.27 from green

---
+++ 05_Java/01_AloneJava/AloneJava/src/main/java/chap15/sec01/exam01/WriteApplication.java
@@ -0,0 +1,29 @@
+package chap15.sec01.exam01;
+
+/**
+ * packageName : chap15.sec01.exam01
+ * fileName : WriteApplication
+ * author : GGG
+ * date : 2023-09-27
+ * description :
+ * 요약 :
+ *       스트림 뜻 : 가상의 통로
+ *       입출력 스트림(InputStream/OutputStream) : 자바에서는 데이터는 스트림을
+ *       통해 입출력이 이루어짐, 읽기용(Input)/쓰기용(Output)에 따라 각자 클래스를
+ *       만듬
+ *       => 대표 4가지 부모 클래스 소개
+ *       InputStream : 1byte 기반 입력(==읽기용), 최상위 추상 클래스
+ *       OutputStream : 1byte 기반 출력(==쓰기용), 최상위 추상 클래스
+ *       Reader : 문자기반(char:2byte) 입력(==읽기용), 최상위 추상 클래스
+ *       Writer : 문자기반(char:2byte) 출력(==쓰기용), 최상위 추상 클래스
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * —————————————————————————————
+ * 2023-09-27         GGG          최초 생성
+ */
+public class WriteApplication {
+    public static void main(String[] args) {
+        
+    }
+}
