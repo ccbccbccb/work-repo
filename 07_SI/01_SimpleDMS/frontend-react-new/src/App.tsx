3c0f9f169b4c82d3d5d7172085a1868172b5ceb9
ccbccbccb <141594822+ccbccbccb@users.noreply.github.com>
2023-10-24 17:48:59 +0900

    10.24 1 from green

---
+++ 07_SI/01_SimpleDMS/frontend-react-new/src/App.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+// app css import
+import "./assets/css/app.css";
+
+import HeaderCom from "./components/common/HeaderCom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import NotFound from "./pages/common/NotFound";
+import DeptListNop from "./pages/dept-nop/DeptListNop";
+import EmpListNop from "./pages/emp-nop/EmpListNop";
+import AddDeptNop from "./pages/dept-nop/AddDeptNop";
+import AddEmpNop from "./pages/emp-nop/AddEmpNop";
+import DeptNop from "./pages/dept-nop/DeptNop";
+import EmpNop from "./pages/emp-nop/EmpNop";
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderCom />
+
+      {/* <!-- 구분 막대 시작 --> */}
+      <div className="gutter text-center text-muted fade-in-box">
+        <div>클론 코딩 예제 사이트에 오신 것을 환영합니다.</div>
+      </div>
+      {/* <!-- 구분 막대 끝 --> */}
+
+      <div id="content-wrapper">
+        {/* 라우터 정의 시작 */}
+        <Routes>
+          {/* login */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+
+          {/* dept */}
+          <Route path="/dept-nop" element={<DeptListNop />} />
+          <Route path="/add-dept-nop" element={<AddDeptNop />} />
+          <Route path="/dept-nop/:dno" element={<DeptNop />} />
+
+
+          {/* emp */}
+          <Route path="/emp-nop" element={<EmpListNop />} />
+          <Route path="/add-emp-nop" element={<AddEmpNop />} />
+          <Route path="/emp-nop/:eno" element={<EmpNop />} />
+
+{/* TODO: 사원 전체 조회 페이지를 만들고 (/emp-nop) 화면에 출력하세요(부서) 
+          1) types/IEmp.ts          (사원 타입)
+          2) services/EmpService.ts (axios 공통함수들)
+          3) pages/emp-nop/EmpListNop.tsx (전체조회 페이지-html 보냄)
+          4) Route 메뉴 달기
+*/}
+
+          {/* NotFound */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        {/* 라우터 정의 끝 */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 07_SI/02_SimpleDMS_Page/frontend-react-new/src/App.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+// app css import
+import "./assets/css/app.css";
+
+import HeaderCom from "./components/common/HeaderCom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import NotFound from "./pages/common/NotFound";
+import DeptList from "./pages/basic/dept/DeptList";
+import EmpList from "./pages/basic/emp/EmpList";
+import AddDept from './pages/basic/dept/AddDept';
+import AddEmp from "./pages/basic/emp/AddEmp";
+import Dept from "./pages/basic/dept/Dept";
+import Emp from "./pages/basic/emp/Emp";
+import QnaList from "./pages/basic/qna/QnaList";
+import CustomerList from "./pages/basic/customer/CustomerList";
+import AddQna from "./pages/basic/qna/AddQna";
+import AddCustomer from "./pages/basic/customer/AddCustomer";
+import Qna from "./pages/basic/qna/Qna";
+import Customer from "./pages/basic/customer/Customer";
+import FaqList from "./pages/normal/faq/FaqList";
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderCom />
+
+      {/* <!-- 구분 막대 시작 --> */}
+      <div className="gutter text-center text-muted fade-in-box">
+        <div>클론 코딩 예제 사이트에 오신 것을 환영합니다.</div>
+      </div>
+      {/* <!-- 구분 막대 끝 --> */}
+
+      <div id="content-wrapper">
+        {/* 라우터 정의 시작 */}
+        <Routes>
+          {/* login */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+
+          {/* dept */}
+          <Route path="/dept" element={<DeptList />} />
+          <Route path="/add-dept" element={<AddDept />} />
+          <Route path="/dept/:dno" element={<Dept />} />
+
+          {/* emp(연습) */}
+          <Route path="/emp" element={<EmpList />} />
+          <Route path="/add-emp" element={<AddEmp />} />
+          <Route path="/emp/:eno" element={<Emp />} />
+
+          {/* qna */}
+          <Route path="/qna" element={<QnaList />} />
+          <Route path="/add-qna" element={<AddQna />} />
+          <Route path="/qna/:qno" element={<Qna />} />
+
+          {/* customer */}
+          <Route path="/customer" element={<CustomerList />} />
+          <Route path="/add-customer" element={<AddCustomer />} />
+          <Route path="/customer/:cid" element={<Customer />} />
+
+          {/* faq */}
+          <Route path="/faq" element={<FaqList />} />
+
+
+          {/* NotFound */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        {/* 라우터 정의 끝 */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
